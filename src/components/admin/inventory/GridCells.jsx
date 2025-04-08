da7d7844cd8e4aa2cc7f3d2a85d93d43f04f41d5
viccast14 <victor.lopez@multimoney.com>
2025-04-08 13:34:04 -0600

    feat: renombrar botón de 'Agregar' a 'Enviar' y actualizar título en historial de entradas

--- src/components/admin/inventory/GridCells.jsx
+++ src/components/admin/inventory/GridCells.jsx
@@ -320,7 +320,7 @@ export const GridCells = ({
                   className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg transition"
                   onClick={handleSend}
                 >
-                  Agregar
+                  Enviar
                 </button>
               </div>
             </div>
--- src/views/history-stock/table/HistoryStock.jsx
+++ src/views/history-stock/table/HistoryStock.jsx
@@ -4,7 +4,7 @@ import { Table } from "./components/Table";
 export const HistoryStock = () => {
   return (
     <div>
-      <Title title="Historial entradas" />
+      <Title title="Historial entradas > Bodega Principal" />
       <Table />
     </div>
   );
